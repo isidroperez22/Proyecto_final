8c2ea56e0b0998c1bb215f080973e9235d113138
isidroperez22 <55449310+isidroperez22@users.noreply.github.com>
2022-12-01 17:42:21 +0100

    Delete src directory

--- src/controller/dashboardController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../model/Transaccion.php";
-
-session_start();
-
-$transaccion = new Transaccion($_SESSION["id"], "", "", "");
-
-echo json_encode($transaccion->mostrar_transacciones());
--- src/controller/logoutController.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Inicia la session en el fichero para poder destruirla
-session_start();
-
-echo session_destroy();
